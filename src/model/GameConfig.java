a4a1d3f282790300c5be7c44a9a0dd55bdd4e789
Matt Eden <mrmatteden@gmail.com>
2020-05-08 13:43:35 +1200

    Create config file for game-wide parameters

---
+++ src/model/GameConfig.java
@@ -0,0 +1,8 @@
+package model;
+
+public class GameConfig {
+    public static final int STARTING_SEEDS = 4;
+    public static final int NUM_HOUSES = 6;
+    public static final int NUM_PLAYERS = 2;
+    public static final int STARTING_PLAYER = 1;
+}
